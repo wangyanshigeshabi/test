e416149a840e5fe92e21d17696b097c3f178b174
wangyanshigeshabi <1843724240@qq.com>
2021-04-19 14:32:54 +0800

    the first

---
+++ fileinfo.py
@@ -0,0 +1,12 @@
+# Script Name       : fileinfo.py
+# Author                : Not sure where I got this from
+# Created               : 28th November 2011
+# Last Modified     :
+# Version               : 1.0
+# Modifications     :
+
+# Description           : Show file information for a given file
+
+
+# get file information using os.stat()
+# tested with Python24 vegsaeat 25sep2006
